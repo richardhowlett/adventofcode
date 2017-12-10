4784e51c7228c36f8cd5b091cbe6cfc056123e80
Richard Howlett <richard.howlett@infotex.co.uk>
2017-12-10 14:31:35 +0000

    - Solution for Day 10 part 1, started on part 2

---
+++ day10.php
@@ -0,0 +1,59 @@
+<?php
+
+if (isset($argv[1])) {
+    $source_string_length = $argv[1];
+} else {
+    $source_string_length = '';
+}
+
+if (isset($argv[2])) {
+    $input = $argv[2];
+} else {
+    $input = '';
+}
+
+$source_string = array();
+
+for ($i = 0; $i < $source_string_length; $i++) {
+    $source_string[] = $i;
+}
+
+//echo ('source_string: ' . implode(' ', $source_string) . "\n");
+
+$input_lengths = explode(',', $input);
+
+$current_position = 0;
+$skip_size = 0;
+foreach ($input_lengths as $input_length) {
+    //echo ('source_string: ' . $source_string . "\n");
+    //echo ('current_position: ' . $current_position . "\n");
+    $tmp_string = array_merge($source_string, $source_string);
+    $string_to_reverse = array_splice($tmp_string, $current_position, $input_length);
+    $reversed_string = array_reverse($string_to_reverse);
+
+    //echo ('input_length: ' . $input_length . "\n");
+    //echo ('string_to_reverse: ' . implode(' ', $string_to_reverse) . "\n");
+    //echo ('reversed_string: ' . implode(' ', $reversed_string) . "\n");
+
+    $update_position = $current_position;
+    for ($i = 0; $i < $input_length; $i++) {
+        if ($update_position >= $source_string_length) {
+            $update_position = 0;
+        }
+
+        //echo ('update position: ' . $update_position . "\n");
+
+        $source_string[$update_position] = $reversed_string[$i];
+
+        $update_position++;
+    }
+
+    $current_position += $input_length + $skip_size;
+    if ($current_position >= $source_string_length) {
+        $current_position = $current_position - $source_string_length;
+    }
+    $skip_size++;
+}
+
+echo ('new source string: ' . implode(' ', $source_string) . "\n");
+echo ('result: ' . ($source_string[0] * $source_string[1]) . "\n");
